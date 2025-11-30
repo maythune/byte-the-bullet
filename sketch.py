def2a697d90cbe2b137f16c4f2248438dad2ffef
tguilu0128 <tguilu0128@gmail.com>
2025-11-30 21:36:55 +0800

    Add files via upload
    look at sketch.py first

---
+++ sketch.py
@@ -0,0 +1,13 @@
+#python -m venv venv
+# For Windows:
+#python -m venv.\venv\Scripts\activate
+# For macOS/Linux:
+#source venv/bin/activate
+
+#pip install requests beautifulsoup4
+# Optionally, for the chatbot framework:
+#pip install chatterbot
+
+#all above needs to be keyed into terminal
+#Python needs to be installed
+#python -m pip install --upgrade pip if there is a need to upgrade pip
